318cbd713a97ac3c47c9f3b9474e64edfeeef9c8
lmjh <lmjhemming@gmail.com>
2022-05-25 18:35:36 +0000

    Add and connect sketchbook home page

--- sketchbook/views.py
+++ sketchbook/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """ A view to display the sketchbook page """
+
+    return render(request, 'sketchbook/index.html')
