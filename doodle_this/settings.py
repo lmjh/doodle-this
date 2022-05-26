7513bc579e015c830837cde653e6741948da4d81
lmjh <lmjhemming@gmail.com>
2022-05-26 17:36:52 +0000

    Build sketchbook left control panel layout

--- doodle_this/settings.py
+++ doodle_this/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5l+45#ee%7l*upyv3q#om68perzc-6x@y8fjy-^a(4@eh0839a'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
