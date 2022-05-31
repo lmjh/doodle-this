a18f011b046e66407484b25c3b20c4c28a36659e
lmjh <lmjhemming@gmail.com>
2022-05-31 09:46:57 +0000

    Organise and clean up code

--- sketchbook/static/sketchbook/js/sketchbook.js
+++ sketchbook/static/sketchbook/js/sketchbook.js
@@ -1,11 +1,12 @@
+// INITIALISE ELEMENTS AND STYLES
+
 // initialise Bootstrap tooltips
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(
-    tooltipTriggerEl))
+    tooltipTriggerEl));
 
 // find sketchbook element
 const canvas = document.querySelector('#sketchbook');
-
 // initialise Atrament canvas
 const sketchbook = new Atrament(canvas, {
     width: 1280,
@@ -27,23 +28,20 @@ Coloris({
 });
 
 // set the cursor element's colour to the value of the Coloris picker
-$('#cursor').css('background-color', document.getElementById('coloris-picker').value)
+$('#cursor').css('background-color', document.getElementById('coloris-picker').value);
 
-// add an event listener to set the Atrament canvas colour and cursor preview colour when a colour is selected with the 
-// Coloris picker
-document.addEventListener('coloris:pick', event => {
-    color = event.detail.color;
-    sketchbook.color = color;
-    $('#cursor').css('background-color', color);
-});
+// call resizeCanvas function
+resizeCanvas();
+
+// DECLARE FUNCTIONS
 
 /**
  * Uses CSS transforms to scale the contents of the #sketchbook-scaler element
  */
 function resizeCanvas() {
     // find sketchbook-scaler and scaler-holder divs
-    let scaler = document.getElementById('sketchbook-scaler')
-    let holder = document.getElementById('scaler-holder')
+    let scaler = document.getElementById('sketchbook-scaler');
+    let holder = document.getElementById('scaler-holder');
 
     // assign sketchbook-holder width to a variable
     let holderWidth = holder.getBoundingClientRect().width;
@@ -58,23 +56,8 @@ function resizeCanvas() {
     scaler.style.transform = 'scale(' + scale + ')';
 
     // set scaler-holder height to height of scaler to fix layout bug
-    $('#scaler-holder').height(scaler.getBoundingClientRect().height)
-};
-
-// call resizeCanvas function
-resizeCanvas();
-
-// add event listener to call resizeCanvas function on window resize
-// to prevent multiple successive resize operations, add a short delay so that resize function is not called until the 
-// user has stopped resizing the window (based on this answer from SO: https://stackoverflow.com/a/15205745)
-let resizeTimer = null
-
-addEventListener("resize", function() {
-    if (resizeTimer != null) window.clearTimeout(resizeTimer);
-    resizeTimer = window.setTimeout(function() {
-        resizeCanvas();
-    }, 200);
-});
+    $('#scaler-holder').height(scaler.getBoundingClientRect().height);
+}
 
 /**
  * Takes a colour as a string and uses it to set the Atrament canvas' current colour
@@ -111,26 +94,6 @@ function findColor(clickedButton) {
         return color;
 }
 
-// add an event listener to the element that holds the preset colour buttons
-document.getElementById("preset-colour-holder").addEventListener("click", function(e) {
-    // if the user clicks on one of the colour buttons inside the holder element
-    if (e.target && e.target.matches(".colour-button")) {
-        // pass the button to the findColor function to get its background colour as an rbg() formatted string
-        let color = findColor(e.target)
-        // pass the returned colour to the changeColor function
-        changeAtramentColor(color);
-        changeColorisColor(color);
-        changeCursorColor(color);
-    }
-})
-
-// add an event listener for the custom event 'colorpicked', triggered when the colour picker tool is used
-sketchbook.addEventListener('colorpicked', function(e) {
-    // use the colour returned by the colorpicked event to update the Coloris plugin and cursor preview 
-    changeColorisColor(e.color);
-    changeCursorColor(e.color);
-})
-
 /**
  * Takes a selected tool button node and sets that tool as the Atrament canvas' current mode.
  * Removes the 'active-tool' class from all tool buttons, then adds it to the selected tool butotn. 
@@ -140,6 +103,60 @@ function changeTool(selectedTool) {
     $('.tool-button').removeClass('active-tool');
     selectedTool.classList.add('active-tool');
 }
+
+/**
+ * Changes the stroke weight of the Atrament canvas to the value of the triggering event's target and updates the cursor
+ * preview to match.
+ */
+function changeWeight(e) {
+    // convert the event taget's value to an int
+    let weight = parseInt(e.target.value);
+    // pass to Atrament canvas
+    sketchbook.weight = weight;
+    // set cursor preview to same width and height as canvas tool
+    $('#cursor').css('width', weight).css('height', weight);
+}
+
+// ADD EVENT LISTENERS
+
+// add an event listener to set the Atrament canvas colour and cursor preview colour when a colour is selected with the 
+// Coloris picker
+document.addEventListener('coloris:pick', event => {
+    let color = event.detail.color;
+    changeAtramentColor(color);
+    changeCursorColor(color);
+});
+
+// add event listener to call resizeCanvas function on window resize.
+// to prevent multiple successive resize operations, add a short delay so that resize function is not called until the 
+// user has stopped resizing the window (based on this answer from SO: https://stackoverflow.com/a/15205745)
+let resizeTimer = null;
+window.addEventListener("resize", function() {
+    if (resizeTimer != null) window.clearTimeout(resizeTimer);
+    resizeTimer = window.setTimeout(function() {
+        resizeCanvas();
+    }, 200);
+});
+
+// add an event listener to the element that holds the preset colour buttons
+document.getElementById("preset-colour-holder").addEventListener("click", function(e) {
+    // if the user clicks on one of the colour buttons inside the holder element
+    if (e.target && e.target.matches(".colour-button")) {
+        // pass the button to the findColor function to get its background colour as an rgb() formatted string
+        let color = findColor(e.target);
+        // pass the returned colour to the changeColor function
+        changeAtramentColor(color);
+        changeColorisColor(color);
+        changeCursorColor(color);
+    }
+});
+
+// add an event listener for the custom event 'colorpicked', triggered when the colour picker tool is used
+sketchbook.addEventListener('colorpicked', function(e) {
+    // use the colour returned by the colorpicked event to update the Coloris plugin and cursor preview 
+    changeColorisColor(e.color);
+    changeCursorColor(e.color);
+});
 
 // add an event listener to the element that holds the tool buttons
 document.getElementById("tool-holder").addEventListener("click", function(e) {
@@ -153,26 +170,12 @@ document.getElementById("tool-holder").addEventListener("click", function(e) {
         // pass the value of the image's parent button element to the changeTool function
         changeTool(e.target.parentElement);
         }
-})
-
-/**
- * Changes the stroke weight of the Atrament canvas to the value of the triggering event's target and updates the cursor
- * preview to match.
- */
-function changeWeight(e) {
-    // convert the event taget's value to an int
-    weight = parseInt(e.target.value);
-    // pass to Atrament canvas
-    sketchbook.weight = weight;
-    // set cursor preview to same width and height as canvas tool
-    $('#cursor').css('width', weight).css('height', weight)
-}
+});
 
 // add event listener to change Atrament canvas stroke weight when slider is changed
-document.getElementById('stroke-weight').addEventListener('change', changeWeight)
+document.getElementById('stroke-weight').addEventListener('change', changeWeight);
 
 // add event listener to clear Atrament canvas when clear confirmation button is clicked
 document.getElementById('clear-sketchbook').addEventListener('click', function() {
     sketchbook.clear();
-})
-
+});
