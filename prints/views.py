556be91187ce9d6b0b17788d1ddc9bc2b5fd6f25
lmjh <lmjhemming@gmail.com>
2022-06-11 16:15:22 +0000

    Build prints page layout

--- prints/views.py
+++ prints/views.py
@@ -3,7 +3,15 @@ from .models import Category, Product, ProductVariant
 
 
 def show_all_prints(request):
+    """
+    A view to display all available print product in the shop
+    """
+
+    products = Product.objects.all()
+
     template = "prints/show_all_prints.html"
-    context = {}
+    context = {
+        'products': products,
+    }
 
     return render(request, template, context)
