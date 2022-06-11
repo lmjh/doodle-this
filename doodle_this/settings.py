cc647de989d07afda49a8329f0732e523427e7b7
lmjh <lmjhemming@gmail.com>
2022-06-11 14:08:33 +0000

    Set up print model admin pages

--- doodle_this/settings.py
+++ doodle_this/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'sketchbook',
     'accounts',
     'notifications',
+    'prints',
 ]
 
 MIDDLEWARE = [
--- prints/admin.py
+++ prints/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Category, Product, ProductVariant
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "display_name",
+        "name",
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "display_name",
+        "name",
+        "category",
+        "variant_type",
+        "description",
+        "image",
+    )
+
+
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display: (
+        "display_name",
+        "name",
+        "product",
+        "sku",
+        "cost",
+        "description",
+        "image",
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductVariant, ProductVariantAdmin)
