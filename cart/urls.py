26ee54f93fd8753bc964125931f4f4aa7a0b583f
lmjh <lmjhemming@gmail.com>
2022-06-15 11:41:45 +0000

    Add ability to add products to cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
+    path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
@@ -8,3 +8,23 @@ def view_cart(request):
 
     template = "cart/cart.html"
     return render(request, template)
+
+
+def add_to_cart(request):
+    if request.method == "POST":
+        variant_id = request.POST.get('variant')
+        quantity = int(request.POST.get('quantity'))
+        drawing = request.POST.get('drawing')
+        redirect_url = request.POST.get('redirect_url')
+
+        cart = request.session.get('cart', {})
+
+        if variant_id in list(cart.keys()):
+            cart[variant_id] += quantity
+        else:
+            cart[variant_id] = quantity
+
+        request.session['cart'] = cart
+
+        print(request.session['cart'])
+        return redirect(redirect_url)
