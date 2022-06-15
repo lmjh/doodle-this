7bc4cb928957eee6b0c1f378094a2a5b41b433ef
lmjh <lmjhemming@gmail.com>
2022-06-15 17:30:28 +0000

    Add feedback messages to cart views

--- cart/views.py
+++ cart/views.py
@@ -27,7 +27,7 @@ def add_to_cart(request):
 
         # set the drawing name to be displayed in messages
         if drawing == "autosave":
-            drawing_name = "your current sketchbook"
+            drawing_name = "Current Doodle"
         else:
             drawing_name = f"Save Slot {drawing}"
 
@@ -41,28 +41,32 @@ def add_to_cart(request):
         for item in cart:
             # if the list contains an object with the same variant id and
             # drawing as the request
-            if item['variant_id'] == variant_id and item['drawing'] == drawing:
+            if item["variant_id"] == variant_id and item["drawing"] == drawing:
                 # increase that object's quantity and set in_cart to true
-                item['quantity'] += quantity
+                item["quantity"] += quantity
                 in_cart = True
                 messages.success(
                     request,
-                    f"Added {quantity} x {variant.product.display_name} - {variant.display_name} printed with {drawing_name} to your cart",
+                    f"Product: {quantity} x {variant.product.display_name} - "
+                    f"{variant.display_name} <br>Doodle: {drawing_name}<br>"
+                    "Added to shopping cart.",
                 )
 
         # if no matching object was found
         if in_cart is False:
             # add an object to the cart using the submitted data
             item = {
-                'variant_id': variant_id,
-                'drawing': drawing,
-                'quantity': quantity,
+                "variant_id": variant_id,
+                "drawing": drawing,
+                "quantity": quantity,
             }
             cart.append(item)
             messages.success(
-                    request,
-                    f"Added {quantity} x {variant.product.display_name} - {variant.display_name} printed with {drawing_name} to your cart",
-                )
+                request,
+                f"Product: {quantity} x {variant.product.display_name} - "
+                f"{variant.display_name} <br>Doodle: {drawing_name}<br>"
+                "Added to shopping cart.",
+            )
 
         request.session["cart"] = cart
         print(request.session["cart"])
@@ -75,41 +79,62 @@ def update_cart_item(request):
     A view to update the quantity of an item in the shopping cart
     """
     if request.method == "POST":
-        variant_id = request.POST.get('variant_id')
-        drawing = request.POST.get('drawing')
-        quantity = int(request.POST.get('quantity'))
+        # gather data from request
+        variant_id = request.POST.get("variant_id")
+        drawing = request.POST.get("drawing")
+        quantity = int(request.POST.get("quantity"))
 
+        # get cart from session
         cart = request.session.get("cart", [])
 
         # iterate over a copy of the cart, enumerate to access loop index
         # (https://stackoverflow.com/a/10665631)
         for index, item in enumerate(cart[:]):
             # find the item matching the variant_id and selected drawing
-            if item['variant_id'] == variant_id and item['drawing'] == drawing:
+            if item["variant_id"] == variant_id and item["drawing"] == drawing:
                 # remove from the cart list if quantity is zero
                 if quantity is 0:
                     cart.remove(item)
+                    messages.success(
+                        request,
+                        "Item removed from shopping cart.",
+                    )
                 # update the cart item's quantity if greater than zero
                 else:
-                    cart[index]['quantity'] = quantity
+                    cart[index]["quantity"] = quantity
+                    messages.success(
+                        request,
+                        "Shopping cart quantity updated.",
+                    )
 
+        # update session cart and redirect to view_cart page
         request.session["cart"] = cart
-        return redirect(reverse('view_cart'))
+        return redirect(reverse("view_cart"))
 
 
 def remove_cart_item(request):
-    if request.method == 'POST':
-        variant_id = request.POST.get('variant_id')
-        drawing = request.POST.get('drawing')
+    """
+    A view to remove an item from the shopping cart
+    """
+    if request.method == "POST":
+        # gather data from request
+        variant_id = request.POST.get("variant_id")
+        drawing = request.POST.get("drawing")
 
+        # get cart from session
         cart = request.session.get("cart", [])
 
         # iterate over a copy of the cart
         for item in cart[:]:
             # find the item matching the variant_id and selected drawing
-            if item['variant_id'] == variant_id and item['drawing'] == drawing:
+            if item["variant_id"] == variant_id and item["drawing"] == drawing:
                 # remove from the cart list
                 cart.remove(item)
+                messages.success(
+                    request,
+                    "Item removed from shopping cart.",
+                )
 
+        # update session cart and redirect to view_cart page
         request.session["cart"] = cart
-        return redirect(reverse('view_cart'))
+        return redirect(reverse("view_cart"))
