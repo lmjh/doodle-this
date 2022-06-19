892d58b668ab21fdc1af50afded5fb871d382e3a
lmjh <lmjhemming@gmail.com>
2022-06-19 11:31:01 +0000

    Configure stripe keys and payment intent

--- doodle_this/settings.py
+++ doodle_this/settings.py
@@ -13,15 +13,18 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+if os.path.exists(".env"):
+    from dotenv import load_dotenv
+    load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -182,3 +185,8 @@ TEST_RUNNER="redgreenunittest.django.runner.RedGreenDiscoverRunner"
 
 # set standard delivery fee for all orders
 STANDARD_DELIVERY_FEE = 3
+
+# stripe configuration
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,19 @@
+from decimal import Decimal
+import stripe
+
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from cart.contexts import cart_contents
 
 
 def checkout(request):
+    # get stripe public and secret keys from environment
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     # get the user's cart from the session
     cart = request.session.get("cart", [])
 
@@ -13,14 +22,34 @@ def checkout(request):
         messages.error(request, "Your cart is empty.")
         return redirect(reverse('show_all_prints'))
 
+    # get the current shopping cart contents
+    current_cart = cart_contents(request)
+
+    # get the grand_total from the current cart and convert from str to decimal
+    total = Decimal(current_cart['grand_total'])
+
+    # set stripe payment total and secret key
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+
+    # create stripe payment intent
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    # show error message if stripe public key missing
+    if not stripe_public_key:
+        messages.error(request, 'Stripe Public Key missing.')
+
     order_form = OrderForm()
 
     template = 'orders/checkout.html'
 
     context = {
         'order_form': order_form,
-        'stripe_public_key': "pk_test_51KwoRQEwpXeQoUfQZvs2fLxxCsrLBUVKEU8ZCeddw395W5nPf4jM88LsfskM07e7VMBcN32WgTsYzJ9OawneFhXD002cDbQyQo",
-        'client_secret': 'test_client_secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
