f5673f90e461d9d4b47dcbcce7e76467d593f11b
lmjh <lmjhemming@gmail.com>
2022-06-22 12:01:34 +0000

    Build pattern functions to construct prompts

--- prompts/patterns.py
+++ prompts/patterns.py
@@ -5,17 +5,18 @@ from django.apps import apps
 
 from .models import Activity, Adjective, Creature, Location
 
+
 # random selection function is based on this method for efficiently selecting
 # random objects from a django model:
 # https://books.agiliq.com/projects/django-orm-cookbook/en/latest/random.html
-
 
 def get_word(model_name):
     """
     Retrieves a randomly selected prompt word from the requested model
     """
-    word_model = apps.get_model('prompts', model_name)
-    # set max_id as the highest assigned id
+    # find the requested model
+    word_model = apps.get_model("prompts", model_name)
+    # set max_id as the highest assigned id in the model
     max_id = word_model.objects.all().aggregate(max_id=Max("id"))["max_id"]
 
     # in case there are deletions (i.e. empty ids) in the database, use a while
@@ -38,68 +39,135 @@ def get_word(model_name):
     return False
 
 
-def get_adjective():
-    """
-    Retrieves a randomly selected adjective from the Adjective database
-    """
-    # set max_id as the highest assigned id
-    max_id = Adjective.objects.all().aggregate(max_id=Max("id"))["max_id"]
-
-    # in case there are deletions (i.e. empty ids) in the database, use a while
-    # loop to attempt 10 times to retrieve a random entry
-    attempts = 0
-    while attempts < 10:
-        # set primary key equal to a random integer between 1 and the max id
-        pk = random.randint(1, max_id)
-        # find the corresponding database entry
-        adjective = Adjective.objects.filter(pk=pk).first()
-
-        if adjective:
-            # if an entry is found, return the entry
-            return adjective
-        else:
-            # if an entry is not found, iterate the attempts counter
-            attempts += 1
-
-    # if no entry was found in 10 attempts, return false
-    return False
-
-
-def get_creature():
-    """
-    Retrieves a randomly selected creature from the Creature database
-    """
-    # set max_id as the highest assigned id
-    max_id = Creature.objects.all().aggregate(max_id=Max("id"))["max_id"]
-
-    # in case there are deletions (i.e. empty ids) in the database, use a while
-    # loop to attempt 10 times to retrieve a random entry
-    attempts = 0
-    while attempts < 10:
-        # set primary key equal to a random integer between 1 and the max id
-        pk = random.randint(1, max_id)
-        # find the corresponding database entry
-        creature = Adjective.objects.filter(pk=pk).first()
-
-        if adjective:
-            # if an entry is found, return the entry
-            return adjective
-        else:
-            # if an entry is not found, iterate the attempts counter
-            attempts += 1
-
-    # if no entry was found in 10 attempts, return false
-    return False
-
-
-
-
 def adjective_creature_activity_location():
-    adjective = random.choice(list(adjectives))
-    creature = random.choice(list(creatures))
-    activity = random.choice(activities)
-    location = random.choice(locations)
-    result = (
-        f"{adjective[0]} {adjective[1]} {creature[1]} {activity} {location}"
-    )
-    return result
+    """
+    Generates and returns a drawing prompt in the format of the function name
+    """
+    # use get_word function to get values for all required words
+    adjective = get_word("Adjective")
+    creature = get_word("Creature")
+    activity = get_word("Activity")
+    location = get_word("Location")
+    # if all values are found
+    if adjective and creature and activity and location:
+        # generate prompt and return
+        result = (
+            f"{adjective.determiner} {adjective.adjective} {creature.creature}"
+            f" {activity.activity} {location.location}"
+        )
+        return result
+    else:
+        # if a get_word failed, return false
+        return False
+
+
+def adjective_creature_activity():
+    """
+    Generates and returns a drawing prompt in the format of the function name
+    """
+    adjective = get_word("Adjective")
+    creature = get_word("Creature")
+    activity = get_word("Activity")
+    # if all values are found
+    if adjective and creature and activity:
+        # generate prompt and return
+        result = (
+            f"{adjective.determiner} {adjective.adjective} {creature.creature}"
+            f" {activity}"
+        )
+        return result
+    else:
+        # if a get_word failed, return false
+        return False
+
+
+def adjective_creature():
+    """
+    Generates and returns a drawing prompt in the format of the function name
+    """
+    adjective = get_word("Adjective")
+    creature = get_word("Creature")
+    # if all values are found
+    if adjective and creature:
+        # generate prompt and return
+        result = (
+            f"{adjective.determiner} {adjective.adjective} {creature.creature}"
+        )
+        return result
+    else:
+        # if a get_word failed, return false
+        return False
+
+
+def creature_activity_location():
+    """
+    Generates and returns a drawing prompt in the format of the function name
+    """
+    creature = get_word("Creature")
+    activity = get_word("Activity")
+    location = get_word("Location")
+    # if all values are found
+    if creature and activity and location:
+        # generate prompt and return
+        result = (
+            f"{creature.determiner} {creature.creature} {activity} "
+            f"{location.location}"
+        )
+        return result
+    else:
+        # if a get_word failed, return false
+        return False
+
+
+def creature_activity():
+    """
+    Generates and returns a drawing prompt in the format of the function name
+    """
+    creature = get_word("Creature")
+    activity = get_word("Activity")
+    # if all values are found
+    if creature and activity:
+        # generate prompt and return
+        result = f"{creature.determiner} {creature.creature} {activity}"
+        return result
+    else:
+        # if a get_word failed, return false
+        return False
+
+
+def adjective_creature_location():
+    """
+    Generates and returns a drawing prompt in the format of the function name
+    """
+    adjective = get_word("Adjective")
+    creature = get_word("Creature")
+    location = get_word("Location")
+    # if all values are found
+    if adjective and creature and location:
+        # generate prompt and return
+        result = (
+            f"{adjective.determiner} {adjective.adjective} {creature.creature}"
+            f" {location.location}"
+        )
+        return result
+    else:
+        # if a get_word failed, return false
+        return False
+
+
+def creature_location():
+    """
+    Generates and returns a drawing prompt in the format of the function name
+    """
+    creature = get_word("Creature")
+    location = get_word("Location")
+    # if all values are found
+    if creature and location:
+        # generate prompt and return
+        result = (
+            f"{creature.determiner} {creature.creature} {location.location}"
+        )
+        return result
+    else:
+        # if a get_word failed, return false
+        return False
