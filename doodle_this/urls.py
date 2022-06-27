90cc6cb3975fceb2d7e7a2469706ca8f4d18ae26
lmjh <lmjhemming@gmail.com>
2022-06-27 07:50:53 +0000

    Build and connect faq page

--- doodle_this/urls.py
+++ doodle_this/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +27,6 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
     path('prompts/', include('prompts.urls')),
+    path('faq/', TemplateView.as_view(template_name="faq.html"), name='faq'),
     path('', include('sketchbook.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
